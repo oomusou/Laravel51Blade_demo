c31936ca5a561563f8782a74b7aa1476674544af
oomusou <oomusou@gmail.com>
2015-12-21 16:39:05 +0800

    1. 修改index.blade.php，將邏輯寫在blade

--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -5,11 +5,18 @@
 </head>
 <body>
 <div>
+    <?php $locale = App::getLocale(); ?>
     @foreach($users as $user)
         <div>
             <h2>{{ $user->name }}</h2>
             <h2>{{ $user->email }}</h2>
-            <h2>{{ $user->created_at }}</h2>
+            @if($locale === 'uk')
+                <h2>{{ $user->created_at->format('d, M, Y') }}</h2>
+            @elseif($locale === 'tw')
+                <h2>{{ $user->created_at->format('Y, m, d') }}</h2>
+            @else
+                <h2>{{ $user->created_at->format('M, d, Y') }}</h2>
+            @endif
         </div>
         <hr>
     @endforeach
